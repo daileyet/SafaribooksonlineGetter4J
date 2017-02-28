fe9344dc62cd705222270ebee9307a1f7652ad1f
Dailey Dai <dailey.yet@gmail.com>
2017-02-28 13:44:12 +0800

    update to relase

--- src/main/java/com/openthinks/others/webpages/WebPagesLaunch.java
+++ src/main/java/com/openthinks/others/webpages/WebPagesLaunch.java
@@ -23,202 +23,210 @@ import com.openthinks.others.webpages.exception.LostConfigureItemException;
 
 /**
  * The web pages download launcher
+ * 
  * @author dailey.yet@outlook.com
  *
  */
 public abstract class WebPagesLaunch {
 
-	protected WebPagesConfigure config;
+  protected WebPagesConfigure config;
 
-	public WebPagesLaunch() {
-		super();
-	}
+  public WebPagesLaunch() {
+    super();
+  }
 
-	public WebPagesLaunch(WebPagesConfigure config) {
-		super();
-		this.config = config;
-	}
+  public WebPagesLaunch(WebPagesConfigure config) {
+    super();
+    this.config = config;
+  }
 
-	/**
-	 * start to download action
-	 * @throws SecurityException
-	 * @throws IOException
-	 */
-	public final void launch() throws SecurityException, IOException {
-		if (config == null)
-			throw new LostConfigureItemException("No configuration.");
-		if (!config.getKeepDir().isPresent())
-			throw new LostConfigureItemException("Lost configuration for save dir.");
-		final WebClient webClient = createWebClient();
-		try {
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-			webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-			webClient.getOptions().setTimeout(35000);
-			if (config.needLogin().isPresent() && config.needLogin().get())
-				loginAndAuth(webClient);
-			travelWholePages(webClient);
-			ProcessLogger.info("All pages has been download.");
-		} catch (Exception e) {
-			ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
-		} finally {
-			if (webClient != null)
-				webClient.close();
-		}
-	}
+  /**
+   * start to download action
+   */
+  public final void launch() throws SecurityException, IOException {
+    if (config == null)
+      throw new LostConfigureItemException("No configuration.");
+    if (!config.getKeepDir().isPresent())
+      throw new LostConfigureItemException("Lost configuration for save dir.");
+    final WebClient webClient = createWebClient();
+    try {
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+      webClient.getOptions().setTimeout(35000);
+      if (config.needLogin().isPresent() && config.needLogin().get())
+        loginAndAuth(webClient);
+      travelWholePages(webClient);
+      ProcessLogger.info("All pages has been download.");
+    } catch (Exception e) {
+      ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
+    } finally {
+      if (webClient != null)
+        webClient.close();
+    }
+  }
 
-	protected void loginAndAuth(WebClient webClient)
-			throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		webClient.getOptions().setJavaScriptEnabled(true);
-		ProcessLogger.info("Open login page...");
-		if (!config.getLoginPageUrl().isPresent())
-			throw new LostConfigureItemException("Lost configuration for login page url.");
-		if (!config.getLoginFormSelector().isPresent())
-			throw new LostConfigureItemException("Lost configuration for login form selector.");
-		if (!config.getLoginAuthInputName().isPresent())
-			throw new LostConfigureItemException("Lost configuration for the input name of login name.");
-		if (!config.getLoginAuthInputValue().isPresent())
-			throw new LostConfigureItemException("Lost configuration for the input value of login name.");
-		if (!config.getLoginAuthPassInputName().isPresent())
-			throw new LostConfigureItemException("Lost configuration for the input name of login password.");
-		if (!config.getLoginAuthPassInputValue().isPresent())
-			throw new LostConfigureItemException("Lost configuration for the input value of login password.");
-		if (!config.getLoginSubmitBtnName().isPresent())
-			throw new LostConfigureItemException("Lost configuration for the input name of login submit.");
+  protected void loginAndAuth(WebClient webClient)
+      throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+    webClient.getOptions().setJavaScriptEnabled(true);
+    ProcessLogger.info("Open login page...");
+    if (!config.getLoginPageUrl().isPresent())
+      throw new LostConfigureItemException("Lost configuration for login page url.");
+    if (!config.getLoginFormSelector().isPresent())
+      throw new LostConfigureItemException("Lost configuration for login form selector.");
+    if (!config.getLoginAuthInputName().isPresent())
+      throw new LostConfigureItemException("Lost configuration for the input name of login name.");
+    if (!config.getLoginAuthInputValue().isPresent())
+      throw new LostConfigureItemException("Lost configuration for the input value of login name.");
+    if (!config.getLoginAuthPassInputName().isPresent())
+      throw new LostConfigureItemException(
+          "Lost configuration for the input name of login password.");
+    if (!config.getLoginAuthPassInputValue().isPresent())
+      throw new LostConfigureItemException(
+          "Lost configuration for the input value of login password.");
+    if (!config.getLoginSubmitBtnName().isPresent())
+      throw new LostConfigureItemException(
+          "Lost configuration for the input name of login submit.");
 
-		final HtmlPage loginPage = webClient.getPage(config.getLoginPageUrl().get());
-		ProcessLogger.debug("Login page load success:" + (loginPage != null));
-		HtmlForm loginForm = null;
-		String formSel = config.getLoginFormSelector().get();
-		DomNodeList<DomNode> elements = loginPage.getBody().querySelectorAll(formSel);
-		ProcessLogger.debug("Login form in login page found(1):" + !elements.isEmpty());
-		loginForm = elements.isEmpty() ? null : (HtmlForm) elements.get(config.getLoginFormIndex());
-		if (loginForm == null) {
-			int index = config.getLoginFormIndex();
-			List<HtmlForm> forms = loginPage.getForms();
-			ProcessLogger.debug("Login form in login page found(2):" + !forms.isEmpty());
-			if (index >= 0 && forms.size() > index) {
-				loginForm = forms.get(0);
-			}
-		}
-		if (loginForm == null) {
-			throw new IllegalArgumentException("Cannot found the login form.");
-		}
+    final HtmlPage loginPage = webClient.getPage(config.getLoginPageUrl().get());
+    ProcessLogger.debug("Login page load success:" + (loginPage != null));
+    HtmlForm loginForm = null;
+    String formSel = config.getLoginFormSelector().get();
+    DomNodeList<DomNode> elements = loginPage.getBody().querySelectorAll(formSel);
+    ProcessLogger.debug("Login form in login page found(1):" + !elements.isEmpty());
+    loginForm = elements.isEmpty() ? null : (HtmlForm) elements.get(config.getLoginFormIndex());
+    if (loginForm == null) {
+      int index = config.getLoginFormIndex();
+      List<HtmlForm> forms = loginPage.getForms();
+      ProcessLogger.debug("Login form in login page found(2):" + !forms.isEmpty());
+      if (index >= 0 && forms.size() > index) {
+        loginForm = forms.get(0);
+      }
+    }
+    if (loginForm == null) {
+      throw new IllegalArgumentException("Cannot found the login form.");
+    }
 
-		HtmlSubmitInput button = loginForm.getInputByName(config.getLoginSubmitBtnName().get());
-		HtmlTextInput userName = loginForm.getInputByName(config.getLoginAuthInputName().get());
-		userName.setValueAttribute(config.getLoginAuthInputValue().get());
-		HtmlPasswordInput userPass = loginForm.getInputByName(config.getLoginAuthPassInputName().get());
-		userPass.setValueAttribute(config.getLoginAuthPassInputValue().get());
-		ProcessLogger.info("Simulate the login action...");
-		button.click();
-		ProcessLogger.info("Login success.");
-	}
+    HtmlSubmitInput button = loginForm.getInputByName(config.getLoginSubmitBtnName().get());
+    HtmlTextInput userName = loginForm.getInputByName(config.getLoginAuthInputName().get());
+    userName.setValueAttribute(config.getLoginAuthInputValue().get());
+    HtmlPasswordInput userPass = loginForm.getInputByName(config.getLoginAuthPassInputName().get());
+    userPass.setValueAttribute(config.getLoginAuthPassInputValue().get());
+    ProcessLogger.info("Simulate the login action...");
+    button.click();
+    ProcessLogger.info("Login success.");
+  }
 
-	protected final void travelWholePages(WebClient webClient) {
-		if (config.getCatalogPageUrl().isPresent()) {
-			catalogResolver(webClient);
-		} else if (config.getStartChainPageUrl().isPresent()) {
-			chainResolver(webClient);
-		} else {
-			ProcessLogger.warn("No url configuration.");
-		}
-	}
+  protected final void travelWholePages(WebClient webClient) {
+    if (config.getCatalogPageUrl().isPresent()) {
+      catalogResolver(webClient);
+    } else if (config.getStartChainPageUrl().isPresent()) {
+      chainResolver(webClient);
+    } else {
+      ProcessLogger.warn("No url configuration.");
+    }
+  }
 
-	protected void catalogResolver(WebClient webClient) {
-		String catalogURL = config.getCatalogPageUrl().get();
-		ProcessLogger.info("Go to download the catalog page:" + catalogURL);
-		if (!config.getPageLinkOfCatalogSelector().isPresent())
-			throw new LostConfigureItemException("Lost configuration for page link selector on catalog page.");
-		try {
-			HtmlPage catalogPage = webClient.getPage(catalogURL);
-			HtmlPageTransfer htmlPageTransfer = getHtmlPageTransfer(catalogPage.cloneNode(true),
-					config.getKeepDir().get());
-			htmlPageTransfer.transfer();
-			DomNodeList<DomNode> links = catalogPage.getBody()
-					.querySelectorAll(config.getPageLinkOfCatalogSelector().get());
+  protected void catalogResolver(WebClient webClient) {
+    String catalogURL = config.getCatalogPageUrl().get();
+    ProcessLogger.info("Go to download the catalog page:" + catalogURL);
+    if (!config.getPageLinkOfCatalogSelector().isPresent())
+      throw new LostConfigureItemException(
+          "Lost configuration for page link selector on catalog page.");
+    try {
+      HtmlPage catalogPage = webClient.getPage(catalogURL);
+      HtmlPageTransfer htmlPageTransfer =
+          getHtmlPageTransfer(catalogPage.cloneNode(true), config.getKeepDir().get());
+      htmlPageTransfer.transfer();
+      DomNodeList<DomNode> links =
+          catalogPage.getBody().querySelectorAll(config.getPageLinkOfCatalogSelector().get());
 
-			links.stream().filter((domNode) -> {
-				DomElement el = (DomElement) domNode;
-				return el.hasAttribute("href") && !el.getAttribute("href").isEmpty()
-						&& !el.getAttribute("href").startsWith("#");
-			}).forEach((domNode) -> {
-				DomElement el = (DomElement) domNode;
-				String relativeUrl = el.getAttribute("href");
-				HtmlPage currentPage = null;
-				ProcessLogger.debug(relativeUrl);
-				try {
-					URL currentUrl = catalogPage.getFullyQualifiedUrl(relativeUrl);
-					ProcessLogger.debug(currentUrl.toString());
-					currentPage = webClient.getPage(currentUrl);
-					ProcessLogger.info("Go to download page:" + currentUrl);
-					HtmlPageTransfer pageTransfer = getHtmlPageTransfer(currentPage, config.getKeepDir().get());
-					pageTransfer.transfer();
-				} catch (Exception e) {
-					ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
-				} finally {
-					if (currentPage != null)
-						currentPage.cleanUp();
-					currentPage = null;
-				}
-			});
-		} catch (FailingHttpStatusCodeException | IOException | LostConfigureItemException e) {
-			ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
-		}
-	}
+      links.stream().filter((domNode) -> {
+        DomElement el = (DomElement) domNode;
+        return el.hasAttribute("href") && !el.getAttribute("href").isEmpty()
+            && !el.getAttribute("href").startsWith("#");
+      }).forEach((domNode) -> {
+        DomElement el = (DomElement) domNode;
+        String relativeUrl = el.getAttribute("href");
+        HtmlPage currentPage = null;
+        ProcessLogger.debug(relativeUrl);
+        try {
+          URL currentUrl = catalogPage.getFullyQualifiedUrl(relativeUrl);
+          ProcessLogger.debug(currentUrl.toString());
+          currentPage = webClient.getPage(currentUrl);
+          ProcessLogger.info("Go to download page:" + currentUrl);
+          HtmlPageTransfer pageTransfer =
+              getHtmlPageTransfer(currentPage, config.getKeepDir().get());
+          pageTransfer.transfer();
+        } catch (Exception e) {
+          ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
+        } finally {
+          if (currentPage != null)
+            currentPage.cleanUp();
+          currentPage = null;
+        }
+      });
+    } catch (FailingHttpStatusCodeException | IOException | LostConfigureItemException e) {
+      ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
+    }
+  }
 
-	/**
-	 * @param htmlPage
-	 * @param file 
-	 * @return
-	 */
-	public abstract HtmlPageTransfer getHtmlPageTransfer(HtmlPage htmlPage, File file);
+  /**
+   * @param htmlPage {@link HtmlPage}
+   * @param file {@link File}
+   * @return {@link HtmlPageTransfer}
+   */
+  public abstract HtmlPageTransfer getHtmlPageTransfer(HtmlPage htmlPage, File file);
 
-	protected void chainResolver(WebClient webClient) {
-		String nextURL = config.getStartChainPageUrl().get();
-		HtmlAnchor nextAnchor = null;
-		ProcessLogger.info("Go to download the start page:" + nextURL);
-		if (!config.getNextChainPageAnchorSelector().isPresent()) {//enhance for no next anchor
-			HtmlPage currentPage;
-			try {
-				currentPage = webClient.getPage(nextURL);
-				HtmlPageTransfer htmlPageTransfer = getHtmlPageTransfer(currentPage, config.getKeepDir().get());
-				htmlPageTransfer.transfer();
-				ProcessLogger.info("Go to download next page:" + nextURL);
-			} catch (FailingHttpStatusCodeException | IOException e) {
-				ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
-			}
-			return;
-			//throw new LostConfigureItemException("Lost configuration for next page link selector on each page.");
-		}
+  protected void chainResolver(WebClient webClient) {
+    String nextURL = config.getStartChainPageUrl().get();
+    HtmlAnchor nextAnchor = null;
+    ProcessLogger.info("Go to download the start page:" + nextURL);
+    if (!config.getNextChainPageAnchorSelector().isPresent()) {// enhance for no next anchor
+      HtmlPage currentPage;
+      try {
+        currentPage = webClient.getPage(nextURL);
+        HtmlPageTransfer htmlPageTransfer =
+            getHtmlPageTransfer(currentPage, config.getKeepDir().get());
+        htmlPageTransfer.transfer();
+        ProcessLogger.info("Go to download next page:" + nextURL);
+      } catch (FailingHttpStatusCodeException | IOException e) {
+        ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
+      }
+      return;
+      // throw new LostConfigureItemException("Lost configuration for next page link selector on
+      // each page.");
+    }
 
-		do {
-			HtmlPage currentPage = null;
-			try {
-				currentPage = webClient.getPage(nextURL);
-				DomNode anchors = currentPage.getBody().querySelector(config.getNextChainPageAnchorSelector().get());
-				nextAnchor = (HtmlAnchor) anchors;
-				if (nextAnchor != null) {// issue for no found next anchor
-					nextURL = currentPage.getFullyQualifiedUrl(nextAnchor.getHrefAttribute()).toString();
-				}
-				HtmlPageTransfer pageTransfer = getHtmlPageTransfer(currentPage, config.getKeepDir().get());
-				pageTransfer.transfer();
-				ProcessLogger.info("Go to download next page:" + nextURL);
-			} catch (Exception e) {
-				ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
-				nextAnchor = null;
-			} finally {
-				if (currentPage != null)
-					currentPage.cleanUp();
-			}
-		} while (nextAnchor != null);
-	}
+    do {
+      HtmlPage currentPage = null;
+      try {
+        currentPage = webClient.getPage(nextURL);
+        DomNode anchors =
+            currentPage.getBody().querySelector(config.getNextChainPageAnchorSelector().get());
+        nextAnchor = (HtmlAnchor) anchors;
+        if (nextAnchor != null) {// issue for no found next anchor
+          nextURL = currentPage.getFullyQualifiedUrl(nextAnchor.getHrefAttribute()).toString();
+        }
+        HtmlPageTransfer pageTransfer = getHtmlPageTransfer(currentPage, config.getKeepDir().get());
+        pageTransfer.transfer();
+        ProcessLogger.info("Go to download next page:" + nextURL);
+      } catch (Exception e) {
+        ProcessLogger.fatal(CommonUtilities.getCurrentInvokerMethod(), e.getMessage());
+        nextAnchor = null;
+      } finally {
+        if (currentPage != null)
+          currentPage.cleanUp();
+      }
+    } while (nextAnchor != null);
+  }
 
-	protected WebClient createWebClient() {
-		if (config.getProxyHost() == null || !config.getProxyHost().isPresent()) {
-			return new WebClient(config.getBrowserVersion());
-		} else {
-			return new WebClient(config.getBrowserVersion(), config.getProxyHost().get(), config.getProxyPort());
-		}
-	}
+  protected WebClient createWebClient() {
+    if (config.getProxyHost() == null || !config.getProxyHost().isPresent()) {
+      return new WebClient(config.getBrowserVersion());
+    } else {
+      return new WebClient(config.getBrowserVersion(), config.getProxyHost().get(),
+          config.getProxyPort());
+    }
+  }
 
 }
--- src/main/java/com/openthinks/others/webpages/agent/ResourceAgent.java
+++ src/main/java/com/openthinks/others/webpages/agent/ResourceAgent.java
@@ -1,27 +1,23 @@
-/**   
- *  Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
-* @Title: ResourceAgent.java 
-* @Description: TODO
-* @author dailey.yet@outlook.com  
-* @date Aug 11, 2015
-* @version V1.0   
-*/
+ * @Title: ResourceAgent.java
+ * @Description: TODO
+ * @author dailey.yet@outlook.com
+ * @date Aug 11, 2015
+ * @version V1.0
+ */
 package com.openthinks.others.webpages.agent;
 
 import java.io.IOException;
@@ -34,86 +30,88 @@ import com.openthinks.others.webpages.util.ResourceType;
 
 /**
  * The agent interface for {@link ResourceKeep}
+ * 
  * @author dailey.yet@outlook.com
  * @see HtmlResourceAgent
  *
  */
 public interface ResourceAgent {
 
-	/**
-	 * the real action to download the resource which represented by the special parameter
-	 * @param element HtmlElement
-	 * @throws IOException
-	 */
-	public void makeKeepToLocal(HtmlElement element) throws IOException;
+  /**
+   * the real action to download the resource which represented by the special parameter
+   * 
+   * @param element HtmlElement
+   * @throws IOException occur IO exception
+   */
+  public void makeKeepToLocal(HtmlElement element) throws IOException;
 
-	/**
-	 * the real action to change link reference of the resource which represented by the special parameter 
-	 * @param element HtmlElement
-	 */
-	public default void makeChangeToLocal(HtmlElement element) {
-		//default nothing to change
-	}
+  /**
+   * the real action to change link reference of the resource which represented by the special
+   * parameter
+   * 
+   * @param element HtmlElement
+   */
+  public default void makeChangeToLocal(HtmlElement element) {
+    // default nothing to change
+  }
 
-	/**
-	 * get the reference {@link ResourceKeep}
-	 * @return T ResourceKeep
-	 */
-	public <T extends ResourceKeep> T getKeeper();
+  /**
+   * get the reference {@link ResourceKeep}
+   * 
+   * @return T ResourceKeep
+   */
+  public <T extends ResourceKeep> T getKeeper();
 
-	/**
-	 * get the resource type
-	 * @return ResourceType
-	 */
-	public default ResourceType getResourceType() {
-		return ResourceType.TEXT;
-	}
+  /**
+   * get the resource type
+   * 
+   * @return ResourceType
+   */
+  public default ResourceType getResourceType() {
+    return ResourceType.TEXT;
+  }
 
-	/**
-	 * the real resource name convert function, it will override the {@link ResourceKeep#resourceNameOf(URL)}
-	 * @param url URL
-	 * @return String
-	 */
-	public default String resolve(URL url) {
-		String name = url.toString();
-		int start = name.lastIndexOf("/");
-		int end = 0;
-		/*
-		end = name.indexOf("?");
-		if (end != -1 && start < end)
-			name = name.substring(start + 1, end);
-		else
-			name = name.substring(start + 1);
-		*/
-		//
-		name = name.substring(start + 1);
-		end = name.lastIndexOf("#");
-		if (end != -1) {
-			name = name.substring(0, end);
-		}
-		name = name.replaceAll("[^0-9A-Za-z_\\-\\.]+", "_");
-		return name;
-	}
+  /**
+   * the real resource name convert function, it will override the
+   * {@link ResourceKeep#resourceNameOf(URL)}
+   * 
+   * @param url URL
+   * @return String
+   */
+  public default String resolve(URL url) {
+    String name = url.toString();
+    int start = name.lastIndexOf("/");
+    int end = 0;
+    /*
+     * end = name.indexOf("?"); if (end != -1 && start < end) name = name.substring(start + 1, end);
+     * else name = name.substring(start + 1);
+     */
+    //
+    name = name.substring(start + 1);
+    end = name.lastIndexOf("#");
+    if (end != -1) {
+      name = name.substring(0, end);
+    }
+    name = name.replaceAll("[^0-9A-Za-z_\\-\\.]+", "_");
+    return name;
+  }
 
-	/**
-	 * xxx.svg#abc
-	 * the real resource name convert function, it will called the {@link HtmlResourceKeeper#getResourceNameOfProundSign()}
-	 * @param url URL
-	 * @return String
-	 */
-	public default String resolveOfPoundSign(URL url) {
-		String name = url.toString();
-		int start = name.lastIndexOf("/");
-		/*		
-		int end = 0;
-		end = name.indexOf("?");
-		if (end != -1 && start < end)
-			name = name.substring(start + 1, end);
-		else
-			name = name.substring(start + 1);
-		*/
-		name = name.substring(start + 1);
-		name = name.replaceAll("[^0-9A-Za-z_\\-\\.]+", "_");
-		return name;
-	}
+  /**
+   * xxx.svg#abc the real resource name convert function, it will called the
+   * {@link HtmlResourceKeeper#getResourceNameOfProundSign()}
+   * 
+   * @param url URL
+   * @return String
+   */
+  public default String resolveOfPoundSign(URL url) {
+    String name = url.toString();
+    int start = name.lastIndexOf("/");
+    /*
+     * int end = 0; end = name.indexOf("?"); if (end != -1 && start < end) name =
+     * name.substring(start + 1, end); else name = name.substring(start + 1);
+     */
+    name = name.substring(start + 1);
+    name = name.replaceAll("[^0-9A-Za-z_\\-\\.]+", "_");
+    return name;
+  }
 }
